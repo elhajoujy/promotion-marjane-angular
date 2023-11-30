139aca8f85b8c7add595ff20af6dbdc0010ca35d
e.elhjoujy <e.elhjoujy@labelvie.ma>
2023-11-30 15:36:04 +0100

    [FEATURE ] [ MRJ-39 ] Dashboard Responsable Rayon pour Accepter ou Refuser les promotions

--- src/app/services/responsable/responsable.service.ts
+++ src/app/services/responsable/responsable.service.ts
@@ -2,10 +2,12 @@ import { Injectable , Inject, OnInit } from '@angular/core';
 import { AuthentificationService } from '../authService';
 import { IUser } from '../../model/user.model';
 import { Promotion } from 'src/app/model/promotion.model';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient , HttpHeaders } from '@angular/common/http';
+
+import { Observable, catchError } from 'rxjs';
 import { Token } from '@angular/compiler';
 import { ResponsablePromotionRequest } from 'src/app/model/ResponsablePromotionRequest.model';
+import { ResponsablePromotionDto } from 'src/app/model/ResponsablePromotionDto.model';
 
 @Injectable({
   providedIn: 'root'
@@ -30,7 +32,7 @@ export class ResponsableService  implements OnInit  {
 
   getAllPromotions() : Observable<Array<Promotion>>
   {
-    var url = "http://localhost:8080/api/v1/responsables-rayon/promotions";
+    const url = "http://localhost:8080/api/v1/responsables-rayon/promotions";
     return this.http.get<Array<Promotion>>(url, {
       headers:{
         "Authorization": `Bearer ${this.authService.getToken()}`
@@ -39,8 +41,25 @@ export class ResponsableService  implements OnInit  {
 
   }
 
-  approuverOuRefuserPromotion(promotionInformation:ResponsablePromotionRequest):boolean{
-    return true;
+  //approuverOuRefuserPromotion(promotionId:number,action:string)
+
+  approuverOuRefuserPromotion(promotionInformation:ResponsablePromotionRequest):Observable<any>{
+    const url = "http://localhost:8080/api/v1/responsables-promotion";
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: 'my-auth-token'
+      })
+    };
+
+    return this.http.post<any>(url,promotionInformation).pipe(
+      catchError((err) => {
+        console.log(err);
+        throw err;
+      }
+    ));
+
   }
+
 
 }
